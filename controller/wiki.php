75bea19a9e3b5b99dc7d834b5728c627c62e6d84
张涛 <358465355@qq.com>
2015-04-28 21:44:07 +0800

    Update wiki.php

--- controller/wiki.php
+++ controller/wiki.php
@@ -5,9 +5,3 @@
  * Date: 15-4-24
  * Time: 上午9:47
  */
-www.duocan.net admin admin888
-afu 12345a
-FTP
-121.199.43.35  ： hyu1538020001  axx5201314
-： hdm150529534_db ： hdm150529534.my3w.com
-： hdm150529534  axx5201314
